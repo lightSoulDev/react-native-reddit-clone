950ab723a1347ac7c7176f1b83ea10266939af45
Нуржан <100024122+lightSoulDev@users.noreply.github.com>
2022-03-02 13:26:59 +0600

    chore: init expo project

---
+++ App.tsx
@@ -0,0 +1,39 @@
+import { NativeBaseProvider } from "native-base";
+import React from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
+import { ActionSheetContextProvider } from "./src/context/ActionSheetContext";
+import PostCard from "./src/redux/components/PostCard";
+
+const App = () => {
+  const isDarkMode = useColorScheme() === "dark";
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? "#2c2c2c" : "#fff",
+  };
+
+  return (
+    <NativeBaseProvider>
+      <SafeAreaView style={backgroundStyle}>
+        <ActionSheetContextProvider>
+          <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={{ width: "100%", height: "100%" }}
+          >
+            <PostCard backgroundColor="#aaa" />
+          </ScrollView>
+        </ActionSheetContextProvider>
+      </SafeAreaView>
+    </NativeBaseProvider>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
